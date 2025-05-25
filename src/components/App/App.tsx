7d2240f311eb752fffc35525201c4df9b346d1e2
MilaOylek <oylekmila@gmail.com>
2025-05-25 22:44:32 +0300

    add styles to button App.module.css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,68 +1,3 @@
-// import { useState } from 'react';
-// import { fetchPhotosByQuery } from '../../services/photo';
-// import type { Photo } from '../../types/photo';
-// import Form from '../Form/Form';
-// import Modal from '../Modal/Modal';
-// import Loader from '../Loader/Loader';
-// import Text from '../Test/Test';
-// import PhotosGallery from '../PhotosGallery/PhotosGallery';
-
-// const App = () => {
-//   const [photos, setPhotos] = useState<Photo[]>([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [isError, setIsError] = useState(false);
-//   const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
-
-//   const handleSearch = async (query: string) => {
-//     if (!query.trim()) return;
-
-//     setIsLoading(true);
-//     setIsError(false);
-
-//     try {
-//       const fetchedPhotos = await fetchPhotosByQuery(query);
-//       setPhotos(fetchedPhotos);
-//     } catch (error) {
-//       console.error(error);
-//       setIsError(true);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   const openModal = (photo: Photo) => {
-//     setSelectedPhoto(photo);
-//     document.body.style.overflow = 'hidden';
-//   };
-
-//   const closeModal = () => {
-//     setSelectedPhoto(null);
-//     document.body.style.overflow = '';
-//   };
-
-//   return (
-//     <div>
-//       <Form onSubmit={handleSearch} />
-
-//       {isLoading && <Loader />}
-
-//       {isError && <Text text="Something went wrong. Try again." />}
-
-//       {photos.length > 0 && (
-//         <PhotosGallery photos={photos} onPhotoClick={openModal} />
-//       )}
-
-//       {selectedPhoto && (
-//         <Modal onClose={closeModal}>
-//           <img src={selectedPhoto.src.original} alt={selectedPhoto.alt} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import { useState } from 'react';
 import { fetchPhotosByQuery } from '../../services/photo';
 import type { Photo } from '../../types/photo';
@@ -71,6 +6,7 @@ import Modal from '../Modal/Modal';
 import Loader from '../Loader/Loader';
 import Text from '../Test/Test';
 import PhotosGallery from '../PhotosGallery/PhotosGallery';
+import styles from './App.module.css';
 
 const App = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
@@ -139,7 +75,7 @@ const App = () => {
         <>
           <PhotosGallery photos={photos} onPhotoClick={openModal} />
           {!isLoading && (
-            <button onClick={loadMore} style={{ marginTop: 20 }}>
+            <button onClick={loadMore} className={styles.loadMoreButton}>
               Load More
             </button>
           )}
